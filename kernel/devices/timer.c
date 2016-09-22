40cbdc3ad63a78984bdae5d97711f114ac5b8264
Thibault <tallancon@gmail.com>
2016-09-22 17:49:08 +0200

    Fixed timer related bug (missing 'volatile' keyword) + Added timer_wait funtion

--- kernel/devices/timer.c
+++ kernel/devices/timer.c
@@ -1,19 +1,42 @@
 #include <stdint.h>
 
 #include <kernel/interrupts.h>
+#include <kernel/io.h>
 
-uint32_t timer_ticks = 0;
+#define COMMAND_REG 0x43
+#define DATA_REG1 0x40
+
+/* We need to use the 'volatile' keyword to stop
+   the compiler from optimizing the variable */
+volatile uint32_t timer_ticks = 0;
+
+void timer_wait(const uint32_t ticks)
+{
+   uint32_t total_ticks = timer_ticks + ticks;
+   while(timer_ticks < total_ticks);
+}
 
 void timer_handler(struct stack *registers)
 {
-   /* The timer fires 18.222 times per second,
-   18 ticks = approximately 1 second */
-
    ++timer_ticks;
 }
 
-/* Set up the system clock by installing the timer handler into IRQ0 */
+void timer_phase(const uint32_t hz)
+{
+   uint32_t divisor = 1193180 / hz;
+
+   write_port(COMMAND_REG, 0x36);
+   /* Set the low byte of 'divisor' */
+   write_port(DATA_REG1, divisor & 0xFF);
+   /* Set the high byte of 'divisor' */
+   write_port(DATA_REG1, divisor >> 8);
+}
+
 void timer_install(void)
 {
+   /* Set up the system clock by installing the timer handler into IRQ0 */
    irq_install_handler(0, timer_handler);
+
+   /* 100Hz */
+   timer_phase(100);
 }
--- kernel/include/timer.h
+++ kernel/include/timer.h
@@ -1,6 +1,7 @@
 #ifndef _KERNEL_TIMER_H
 #define _KERNEL_TIMER_H
 
+void timer_wait(const uint32_t ticks);
 void timer_install(void);
 
 #endif
