f8ae4614c0e130efcb62dd46d0cfbee52bc1d352
Thibault <tallancon@gmail.com>
2016-09-24 19:39:27 +0200

    Improved keyboard layout

--- kernel/devices/keyboard.c
+++ kernel/devices/keyboard.c
@@ -1,64 +1,167 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <kernel/interrupts.h>
 #include <kernel/io.h>
 
 #define KEYBOARD_DATA 0x60
 
-/* Basic US keyboard layout */
-unsigned char us_layout[128] = {
-   0, 27,
-   '1','2','3','4','5','6','7','8','9','0',
-   '-','=','\b',
-   '\t', /* tab */
-   'q','w','e','r','t','y','u','i','o','p','[',']','\n',
-   0, /* control */
-   'a','s','d','f','g','h','j','k','l',';','\'', '`',
-   0, /* left shift */
-   '\\','z','x','c','v','b','n','m',',','.','/',
-   0, /* right shift */
-   '*',
-   0, /* alt */
-   ' ', /* space */
-   0, /* caps lock */
-   0, /* F1 [59] */
-   0, 0, 0, 0, 0, 0, 0, 0,
-   0, /* ... F10 */
-   0, /* 69 num lock */
-   0, /* scroll lock */
-   0, /* home */
-   0, /* up */
-   0, /* page up */
-   '-',
-   0, /* left arrow */
-   0,
-   0, /* right arrow */
-   '+',
-   0, /* 79 end */
-   0, /* down */
-   0, /* page down */
-   0, /* insert */
-   0, /* delete */
-   0, 0, 0,
-   0, /* F11 */
-   0, /* F12 */
-   0, /* everything else */
+#define LSHIFT 0x29
+#define RSHIFT 0x35
+#define CTRL   0x1C
+#define ALT    0x37
+
+/* US keyboard layout */
+unsigned char us_layout[] = {
+   /*    SHIFT CTRL  ALT */
+   0x1B, 0x1B, 0x1B, 0x1B, /* esc (0x01) */
+   '1', '!', '1', '1',
+   '2', '@', '2', '2',
+   '3', '#', '3', '3',
+   '4', '$', '4', '4',
+   '5', '%', '5', '5',
+   '6', '^', '6', '6',
+   '7', '&', '7', '7',
+   '8', '*', '8', '8',
+   '9', '(', '9', '9',
+   '0', ')', '0', '0',
+   '-', '_', '-', '-',
+   '=', '+', '=', '=',
+   0x08, 0x08, 0x7F, 0x08, /* backspace */
+   0x09, 0x09, 0x09, 0x09, /* tab */
+   'q', 'Q', 'q', 'q',
+   'w', 'W', 'w', 'w',
+   'e', 'E', 'e', 'e',
+   'r', 'R', 'r', 'r',
+   't', 'T', 't', 't',
+   'y', 'Y', 'y', 'y',
+   'u', 'U', 'u', 'u',
+   'i', 'I', 'i', 'i',
+   'o', 'O', 'o', 'o',
+   'p', 'P', 'p', 'p',
+   '[', '{', '[', '[',
+   ']', '}', ']', ']',
+   0x0A, 0x0A, 0x0A, 0x0A, /* enter */
+   0xFF, 0xFF, 0xFF, 0xFF, /* ctrl */
+   'a', 'A', 'a', 'a',
+   's', 'S', 's', 's',
+   'd', 'D', 'd', 'd',
+   'f', 'F', 'f', 'f',
+   'g', 'G', 'g', 'g',
+   'h', 'H', 'h', 'h',
+   'j', 'J', 'j', 'j',
+   'k', 'K', 'k', 'k',
+   'l', 'L', 'l', 'l',
+   ';', ':', ';', ';',
+   0x27, 0x22, 0x27, 0x27, /* '" */
+   '`', '~', '`', '`',     /* `~ */
+   0xFF, 0xFF, 0xFF, 0xFF, /* Lshift (0x2a) */
+   '\\', '|', '\\', '\\',
+   'z', 'Z', 'z', 'z',
+   'x', 'X', 'x', 'x',
+   'c', 'C', 'c', 'c',
+   'v', 'V', 'v', 'v',
+   'b', 'B', 'b', 'b',
+   'n', 'N', 'n', 'n',
+   'm', 'M', 'm', 'm',
+   0x2C, 0x3C, 0x2C, 0x2C, /* ,< */
+   0x2E, 0x3E, 0x2E, 0x2E, /* .> */
+   0x2F, 0x3F, 0x2F, 0x2F, /* /? */
+   0xFF, 0xFF, 0xFF, 0xFF, /* Rshift (0x36) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x37) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x38) */
+   ' ', ' ', ' ', ' ',     /* space  */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3a) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3b) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3c) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3d) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3e) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x3f) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x40) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x41) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x42) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x43) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x44) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x45) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x46) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x47) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x48) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x49) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4a) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4b) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4c) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4d) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4e) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x4f) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x50) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x51) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x52) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x53) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x54) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x55) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x56) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x57) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x58) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x59) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5a) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5b) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5c) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5d) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5e) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x5f) */
+   0xFF, 0xFF, 0xFF, 0xFF, /* (0x60) */
+   0xFF, 0xFF, 0xFF, 0xFF  /* (0x61) */
 };
 
 void keyboard_handler(struct stack *registers)
 {
    uint8_t scancode;
+   uint16_t index;
+   static uint8_t offset = 0;
 
    scancode = read_port(KEYBOARD_DATA);
+   --scancode;
 
-   /* If the top bit of the byte we read from the keyboard is
-      set, that means that a key has just been released */
-   if(scancode & 0x80) {
+   /* Key pressed */
+   if(scancode < 0x80) {
+      switch(scancode) {
+         case LSHIFT:
+            offset = 1;
+            break;
+         case RSHIFT:
+            offset = 1;
+            break;
+         case CTRL:
+            offset = 2;
+            break;
+         case ALT:
+            offset = 3;
+            break;
+         default:
+            index = scancode * 4 + offset;
+            putchar(us_layout[index]);
+            break;
+      }
    }
-   /* Normal key press */
-   else
-      putchar(us_layout[scancode]);
+   /* Key released */
+   else {
+      scancode -= 0x80;
+      switch(scancode) {
+         case LSHIFT:
+            offset = 0;
+            break;
+         case RSHIFT:
+            offset = 0;
+            break;
+         case CTRL:
+            offset = 0;
+            break;
+         case ALT:
+            offset = 0;
+            break;
+      }
+   }
 }
 
 void keyboard_install(void)
