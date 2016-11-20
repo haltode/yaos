404af416aa9e0372e2afb93220722e7872bcbc0e
Thibault <tallancon@gmail.com>
2016-11-20 10:58:40 +0100

    Cleaned up some comments

--- kernel/memory/virtual_memory.c
+++ kernel/memory/virtual_memory.c
@@ -33,7 +33,6 @@ void virt_mem_init(void)
    /* Identity map the first 4 MiB (our kernel starts at 1 MiB) */
    uint32_t phys_addr = 0x0;
    for(size_t i = 0; i < ENTRY_PER_TABLE; ++i) {
-      /* Create the page and set it */
       uint32_t *page = &identity_table->entry[i];
       pt_entry_set_frame(page, phys_addr);
       pt_entry_add_flags(page, PTE_PRESENT_BIT);
@@ -61,12 +60,10 @@ void virt_mem_init(void)
 
 bool virt_mem_alloc_page(uint32_t *pt_entry)
 {
-   /* Allocate the new frame */
    void *frame = phys_mem_alloc_frame();
    if(!frame)
       return false;
 
-   /* Set up the frame in the page table entry */
    pt_entry_set_frame(pt_entry, (uint32_t)frame);
    pt_entry_add_flags(pt_entry, PTE_PRESENT_BIT);
 
@@ -75,12 +72,10 @@ bool virt_mem_alloc_page(uint32_t *pt_entry)
 
 void virt_mem_free_page(uint32_t *pt_entry)
 {
-   /* Free the frame */
    void *frame = (void *) pt_entry_get_frame(*pt_entry);
    if(frame)
       phys_mem_free_frame(frame);
 
-   /* Clear the page table entry */
    pt_entry_del_flags(pt_entry, PTE_PRESENT_BIT);
 }
 
@@ -90,23 +85,16 @@ void virt_mem_free_page(uint32_t *pt_entry)
 
 void virt_mem_map_page(void *physical, void *virtual)
 {
-   /* Get the page directory */
    Page_dir *dir = virt_mem_get_dir();
-
-   /* Get the page directory entry */
    uint32_t dir_index = pd_index((uint32_t) virtual);
    uint32_t *pd_entry = &dir->entry[dir_index];
 
    /* Check if the page table is valid (allocated and present) */
    if((*pd_entry & PTE_PRESENT_BIT) != PTE_PRESENT_BIT) {
       /* The page table is not valid, we need to create one */
-
-      /* Allocate the table */
       Page_table *new_table = (Page_table *) phys_mem_alloc_frame();
       if(!new_table)
          return;
-
-      /* Clear the table */
       memset(new_table, 0, sizeof(Page_table));
 
       /* Set up the new page directory entry */
@@ -116,10 +104,8 @@ void virt_mem_map_page(void *physical, void *virtual)
       pd_entry_set_frame(new_pd_entry, (uint32_t) new_table);
    }
 
-   /* Get the page table */
+   /* Get the corresponding page table and page */
    Page_table *table = (Page_table *) pd_entry_phys_addr(pd_entry);
-
-   /* Get the page */
    uint32_t *page = &table->entry[pt_index((uint32_t) virtual)];
 
    /* Map the page and mark it as present */
@@ -131,19 +117,14 @@ void virt_mem_map_page(void *physical, void *virtual)
 
 void virt_mem_unmap_page(void *virtual)
 {
-   /* Get the page directory */
    Page_dir *dir = virt_mem_get_dir();
-
-   /* Get the page directory entry */
    uint32_t dir_index = pd_index((uint32_t) virtual);
    uint32_t *pd_entry = &dir->entry[dir_index];
 
    /* Check if the page table is valid (allocated and present) */
    if((*pd_entry & PTE_PRESENT_BIT) == PTE_PRESENT_BIT) {
-      /* Get the page table */
+      /* Get the corresponding page table and page */
       Page_table *table = (Page_table *) pd_entry_phys_addr(pd_entry);
-
-      /* Get the page */
       uint32_t *page = &table->entry[pt_index((uint32_t) virtual)];
 
       /* Unmap the page */
